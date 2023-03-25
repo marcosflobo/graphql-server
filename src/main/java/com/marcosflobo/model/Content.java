7db024690f217667c26aeab49219587881197255
Marcos F. Lobo <hello@marcosflobo.com>
2023-03-25 17:35:14 +0100

    Add data model classes

---
+++ src/main/java/com/marcosflobo/model/Content.java
@@ -0,0 +1,26 @@
+package com.marcosflobo.model;
+
+public class Content {
+
+  private final String id;
+  private final String title;
+  private final ContentJSON content;
+
+  public Content(String id, String title, ContentJSON content) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public ContentJSON getContent() {
+    return content;
+  }
+}
